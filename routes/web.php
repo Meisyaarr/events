50df7580397c2b462648822a52f0d236631ed23c
Meisyaarr <meisyayuniar81@gmail.com>
2020-09-10 22:10:17 -0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,15 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['prefix' => 'activitys'], function(){
+    Route::get('index', 'ActivityController@index')->name('activitys');
+    Route::get('create','ActivityController@create')->name('activitys.create');
+});
+Route::group(['prefix' => 'registers'], function(){
+    Route::get('index', 'Register\RegistersController@index')->name('registers');
+    Route::get('ambil-formulir', 'Register\RegistersController@create')->name('registers.ambil-formulir');
+});
+
+Route::group(['prefix' => 'users'], function(){
+    Route::get('index', 'User\UserController@index')->name('users');
+});
