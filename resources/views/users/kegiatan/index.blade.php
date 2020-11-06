2f897f0724b6fc7afae5105590e448953e4ad4b8
Meisyaarr <meisyayuniar81@gmail.com>
2020-11-06 10:10:03 +0700

    design kegiatan

---
+++ resources/views/users/kegiatan/index.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container py-3">
+    <nav aria-labe;="breadcrumb">
+        <ol class="breadcrumb bg-white">
+            <li class="breadcrumb-item active" aria-current="page">
+                <li class="badge badge-info">
+                    Aktif
+                </li>
+            </li>
+        </ol>
+    </nav>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card border-0">
+                <div class="card-body">
+                    <div class="d-flex">
+                        <div>
+                            <img src="{{asset('banner/crybaby.jpg')}}" alt="" class="rounded-circle"  width="80" height="80" srcset="">
+                        </div>
+                        <div>
+                            <h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h3>
+                            <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                            <button type="button" class="btn btn-outline-primary btn-block">Daftar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-12">
+            <div class="card border-0">
+                <div class="card-body">
+                    <div class="d-flex">
+                        <div>
+                            <img src="{{asset('banner/crybaby.jpg')}}" alt="" class="rounded-circle"  width="80" height="80" srcset="">
+                        </div>
+                        <div>
+                            <h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h3>
+                            <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                            <button type="button" class="btn btn-outline-primary btn-block">Daftar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-12">
+            <div class="card border-0">
+                <div class="card-body">
+                    <div class="d-flex">
+                        <div>
+                            <img src="{{asset('banner/crybaby.jpg')}}" alt="" class="rounded-circle"  width="80" height="80" srcset="">
+                        </div>
+                        <div>
+                            <h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h3>
+                            <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                            <button type="button" class="btn btn-outline-primary btn-block">Daftar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-12">
+            <div class="card border-0">
+                <div class="card-body">
+                    <div class="d-flex">
+                        <div>
+                            <img src="{{asset('banner/crybaby.jpg')}}" alt="" class="rounded-circle"  width="80" height="80" srcset="">
+                        </div>
+                        <div>
+                            <h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h3>
+                            <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                            <button type="button" class="btn btn-outline-primary btn-block">Daftar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
